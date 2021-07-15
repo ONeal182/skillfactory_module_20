e3037a86e7ba6d81bc2fc0aa5ccfe603db54dc70
Vladimir <vladimir@rupor.agency>
2021-07-15 16:35:38 +0300

    add reg

--- src/app.js
+++ src/app.js
@@ -3,15 +3,35 @@ import "./styles/style.css";
 import taskFieldTemplate from "./templates/taskField.html";
 import noAccessTemplate from "./templates/noAccess.html";
 import { User } from "./models/User";
-import { generateTestUser } from "./utils";
+import { Task } from "./models/Task";
+import { regUser } from "./utils";
 import { State } from "./state";
 import { authUser } from "./services/auth";
 
 export const appState = new State();
-
+sessionStorage.clear();
 const loginForm = document.querySelector("#app-login-form");
+const regBtn = document.querySelector('#app-regist-btn');
+const addTsk = "<input class='add-new-task' type='text'>";
+let auth = false;
 
-generateTestUser(User);
+
+regBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(loginForm);
+  const login = formData.get("login");
+  const password = formData.get("password");
+  regUser(User, login, password);
+
+})
+const addHtmlTask = (task) => {
+  let innerTask = document.querySelector(`.task-items[data-state = "${task.state}"] `);
+  let newTask = document.createElement('li');
+  newTask.className = "task-item";
+  newTask.innerHTML = task.text;
+  innerTask.previousElementSibling.append(newTask);
+
+}
 
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -24,4 +44,42 @@ loginForm.addEventListener("submit", function (e) {
     : noAccessTemplate;
 
   document.querySelector("#content").innerHTML = fieldHTMLContent;
+  if (authUser(login, password)) {
+    auth = true;
+    const task = new Task(login);
+    let taskAll = task.getTask();
+    const addTasks = (elObj) => {
+      taskAll.forEach(Element =>
+        addHtmlTask(Element)
+          
+
+        )
+    }
+    
+    addTasks(taskAll);
+    const btnCard = document.querySelectorAll('.add-card');
+    btnCard.forEach(Element =>
+      Element.addEventListener('click', () => {
+
+        let newcontent = document.createElement('li', 'input');
+        newcontent.className = "task-item";
+        newcontent.innerHTML = addTsk;
+        Element.previousElementSibling.append(newcontent);
+        console.log(addTask(taskAll));
+      })
+    )
+
+  }
 });
+const addTask = (task) => {
+  if (auth) {
+    let inputAddTask = document.querySelectorAll('.add-new-task');
+    inputAddTask.forEach(Element =>
+      Element.addEventListener('blur', () => {
+          return Element.value;
+      })
+    )
+  }
+
+}
+
--- src/models/User.js
+++ src/models/User.js
@@ -19,7 +19,76 @@ export class User extends BaseModel {
   }
   static save(user) {
     try {
+
       addToStorage(user, user.storageKey);
+      let userObj = {
+        login: user.login, 
+        password: user.password, 
+        id: user.id, 
+        task:[ 
+          {
+            text: 'Login page – performance issues',
+            state: 'Backlog'
+          },
+          {
+            text: 'Sprint bugfix',
+            state: 'Backlog'
+          },
+          {
+            text: 'Shop page – performance issues',
+            state: 'Ready'
+          },
+          {
+            text: 'Checkout bugfix',
+            state: 'Ready'
+          },
+          {
+            text: 'Shop bug1',
+            state: 'Ready'
+          },
+          {
+            text: 'Shop bug2',
+            state: 'Ready'
+          },
+          {
+            text: 'Shop bug3',
+            state: 'Ready'
+          },
+          {
+            text: 'Shop bug4',
+            state: 'Ready'
+          },
+          {
+            text: 'User page – performance issues',
+            state: 'InProgress'
+          },
+          {
+            text: 'Auth bugfix',
+            state: 'InProgress'
+          },
+          {
+            text: 'Main page – performance issues',
+            state: 'Finished'
+          },
+          {
+            text: 'Main page bugfix',
+            state: 'Finished'
+          },
+          
+        ] };
+      let userJson = JSON.stringify(userObj);
+      let usserArray = []
+      if (sessionStorage.getItem('user') === null) {
+        usserArray.push(JSON.parse(userJson));
+
+        sessionStorage.setItem('user', JSON.stringify(usserArray));
+
+      }else{
+        let newUser = JSON.parse(userJson);
+        let objUser = JSON.parse(sessionStorage.getItem('user'));
+        objUser.push(newUser);
+        sessionStorage.setItem('user', JSON.stringify(objUser));
+      }
       return true;
     } catch (e) {
       throw new Error(e);
