11533d43f9f8134a94fcc3e6a834dc113ad353a1
Vladimir <vladimir@rupor.agency>
2021-07-15 17:31:37 +0300

    new task

--- src/app.js
+++ src/app.js
@@ -15,6 +15,21 @@ const regBtn = document.querySelector('#app-regist-btn');
 const addTsk = "<input class='add-new-task' type='text'>";
 let auth = false;
 
+const addTaskHtml = (task, btnData) => {
+  let taskText;
+  if (auth) {
+    let inputAddTask = document.querySelectorAll('.add-new-task');
+    inputAddTask.forEach(Element =>
+      Element.addEventListener('blur', () => {
+          taskText = Element.value;
+          let newTask = {text:taskText,state: btnData};
+          task.push(newTask)
+          addNewHtmlTask(newTask);
+      })
+    )
+    
+  }
+}
 
 regBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -24,8 +39,17 @@ regBtn.addEventListener("click", function (e) {
   regUser(User, login, password);
 
 })
-const addHtmlTask = (task) => {
+const addNewHtmlTask = (task) =>{
   let innerTask = document.querySelector(`.task-items[data-state = "${task.state}"] `);
+  innerTask.textContent = '';
+  let newTask = document.createElement('li');
+  newTask.className = "task-item";
+  newTask.innerHTML = task.text;
+  innerTask.previousElementSibling.append(newTask);
+}
+const addAllHtmlTask = (task) => {
+  let innerTask = document.querySelector(`.task-items[data-state = "${task.state}"] `);
+  innerTask.textContent = '';
   let newTask = document.createElement('li');
   newTask.className = "task-item";
   newTask.innerHTML = task.text;
@@ -50,7 +74,7 @@ loginForm.addEventListener("submit", function (e) {
     let taskAll = task.getTask();
     const addTasks = (elObj) => {
       taskAll.forEach(Element =>
-        addHtmlTask(Element)
+        addAllHtmlTask(Element)
           
 
         )
@@ -60,26 +84,18 @@ loginForm.addEventListener("submit", function (e) {
     const btnCard = document.querySelectorAll('.add-card');
     btnCard.forEach(Element =>
       Element.addEventListener('click', () => {
-
+        let btnData = Element.dataset.btn;
         let newcontent = document.createElement('li', 'input');
         newcontent.className = "task-item";
         newcontent.innerHTML = addTsk;
         Element.previousElementSibling.append(newcontent);
-        console.log(addTask(taskAll));
+        let text = addTaskHtml(taskAll, btnData);
+        console.log(text);
       })
     )
 
   }
 });
-const addTask = (task) => {
-  if (auth) {
-    let inputAddTask = document.querySelectorAll('.add-new-task');
-    inputAddTask.forEach(Element =>
-      Element.addEventListener('blur', () => {
-          return Element.value;
-      })
-    )
-  }
 
-}
+
 
