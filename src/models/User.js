c1027febb3e09f48d4cdcd79ceeb4cf922536124
Vladimir <vladimir@rupor.agency>
2021-07-17 12:30:15 +0300

    add new task function

--- src/models/User.js
+++ src/models/User.js
@@ -27,50 +27,62 @@ export class User extends BaseModel {
         id: user.id, 
         task:[ 
           {
+            id: 1,
             text: 'Login page – performance issues',
             state: 'Backlog'
           },
           {
+            id: 2,
             text: 'Sprint bugfix',
             state: 'Backlog'
           },
           {
+            id: 3,
             text: 'Shop page – performance issues',
             state: 'Ready'
           },
           {
+            id: 4,
             text: 'Checkout bugfix',
             state: 'Ready'
           },
           {
+            id: 5,
             text: 'Shop bug1',
             state: 'Ready'
           },
           {
+            id: 6,
             text: 'Shop bug2',
             state: 'Ready'
           },
           {
+            id: 7,
             text: 'Shop bug3',
             state: 'Ready'
           },
           {
+            id: 8,
             text: 'Shop bug4',
             state: 'Ready'
           },
           {
+            id: 9,
             text: 'User page – performance issues',
             state: 'InProgress'
           },
           {
+            id: 10,
             text: 'Auth bugfix',
             state: 'InProgress'
           },
           {
+            id: 11,
             text: 'Main page – performance issues',
             state: 'Finished'
           },
           {
+            id: 12,
             text: 'Main page bugfix',
             state: 'Finished'
           },
