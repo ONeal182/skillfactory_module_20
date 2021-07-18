a8f8517f996822b4d723a91aa3fa07b842f45adf
Vladimir <vladimir@rupor.agency>
2021-07-18 19:24:36 +0300

    add class refactor

---
+++ src/Render.js
@@ -0,0 +1,121 @@
+export class Render {
+    constructor() {
+
+    }
+    addHtmlElement(task) {
+        let innerTask = document.querySelector(`.task-items[data-state = "${task.state}"] `);
+        let newTask = document.createElement('li');
+        newTask.className = "task-item";
+        newTask.innerHTML = task.name;
+        newTask.dataset.id = task.id;
+        innerTask.append(newTask);
+    }
+    addHtmlSelected = (task, stateObj, prevElement) => {
+        let innerTask = document.querySelector(`.task-items[data-state = "${prevElement}"] `);
+        let newTask = document.createElement('li');
+        let stateTask = ``;
+        task.forEach((Element) => {
+            newTask.className = "task-item";
+            if (Element.state == stateObj) {
+                stateTask += ` <option class="task-item_option" data-id="${Element.id}" value="${Element.name}">${Element.name}</option>`;
+            }
+        });
+        newTask.innerHTML = `<select class="task-item_select" name="" id="">${stateTask}</select>`;
+        innerTask.append(newTask);
+    }
+    addTasks = (takeAll) => {
+        takeAll.forEach(Element =>
+            this.addHtmlElement(Element)
+        )
+    }
+    clearAll = (htmlContent) => {
+        htmlContent.forEach(Element =>
+            Element.innerHTML = ''
+        );
+    }
+    addDescription(taskobj) {
+        let inputAddTask = document.querySelectorAll(`.task-items .task-item`);
+        let task = taskobj;
+        inputAddTask.forEach(Element => {
+            Element.addEventListener('dblclick', function test(e) {
+                task.forEach((Element1) => {
+                    if (Number(Element1.id) === Number(Element.dataset.id)) {
+
+                        let changeDiscription = document.querySelector(`.changeDiscription`);
+                        let changeDiscriptionTextarea = document.querySelector(`.changeDiscription .changeTextare`);
+                        let changeDiscriptionText = document.querySelector(`.changeDiscription .changeDiscriptionText`);
+                        let nameTask = document.querySelector(`.changeDiscription .nameTask`);
+                        let taskTextSubmit = document.querySelector(`.task-text-submit`);
+                        let close = document.querySelector(`.close`);
+                        changeDiscriptionTextarea.value = '';
+                        changeDiscription.style.display = 'flex';
+                        nameTask.textContent = Element1.name;
+
+                        if (Element1.text != '') {
+                            changeDiscriptionText.textContent = Element1.text;
+                            changeDiscriptionText.style.display = 'block';
+                            changeDiscriptionTextarea.value = '';
+                        }
+                        close.addEventListener('click', () => {
+                            changeDiscription.style.display = 'none';
+                            changeDiscriptionText.textContent = '';
+                            nameTask.textContent = '';
+                        })
+                        taskTextSubmit.addEventListener('click', function test2(e) {
+                            Element1.text = changeDiscriptionTextarea.value;
+                            e.currentTarget.removeEventListener('click', test2);
+
+                        })
+                    }
+                })
+            })
+        })
+    }
+    transferTask(task, datTask) {
+        let taskText;
+        let inputAddTask = document.querySelectorAll('.task-submit');
+        let selectedTask = document.querySelector(`.task-items[data-state=${datTask}] .task-item_select`);
+        let innerTask = document.querySelectorAll(`.task-items`);
+        let addCard = document.querySelector(`.add-card[data-btn = ${datTask}]`)
+
+        inputAddTask.forEach(Element => {
+            Element.addEventListener('click', function changeStateHandler(e) {
+                this.clearAll(innerTask);
+                taskText = selectedTask.value;
+                let taskTextId = selectedTask.options[selectedTask.selectedIndex].dataset.id;
+                task.forEach((Element2) => {
+                    if (Element2.id == taskTextId) {
+                        Element2.state = datTask;
+                    }
+                }
+
+                )
+                task.forEach(Element1 =>
+                    this.addHtmlElement(Element1)
+                )
+                Element.classList.add('task-submit_hidden');
+                selectedTask.parentElement.remove();
+                addCard.classList.remove('task-submit_hidden');
+                e.currentTarget.removeEventListener('click', changeStateHandler);
+                this.addDescription(task);
+            }, false)
+
+        })
+    }
+    addTaskHtml(task, btnData) {
+        let taskText;
+        let inputAddTask = document.querySelectorAll('.add-new-task');
+        inputAddTask.forEach(Element =>
+            Element.addEventListener('blur', (e) => {
+
+                taskText = Element.value;
+                let newTask = { id: task.length + 1, name: taskText, state: btnData, text: '' };
+                task.push(newTask)
+                this.addHtmlElement(newTask);
+                Element.parentElement.remove();
+                this.addDescription(task);
+
+            })
+        )
+    }
+}
--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ import { regUser } from "./utils";
 import { State } from "./state";
 import { authUser } from "./services/auth";
 import { v4 as uuid } from "uuid";
-
+import { Render } from "./Render.js";
+let render = new Render();
 export const appState = new State();
 sessionStorage.clear();
 const loginForm = document.querySelector("#app-login-form");
@@ -17,135 +18,18 @@ const addTsk = "<input class='add-new-task' type='text'>";
 let auth = false;
 
 const addTaskHtml = (task, btnData) => {
-  let taskText;
   if (auth) {
-    let inputAddTask = document.querySelectorAll('.add-new-task');
-    inputAddTask.forEach(Element =>
-      Element.addEventListener('blur', (e) => {
-
-        taskText = Element.value;
-        let newTask = { id: task.length + 1, name: taskText, state: btnData, text: '' };
-        task.push(newTask)
-        addNewHtmlTask(newTask);
-        Element.parentElement.remove();
-        addDescription(task);
-
-      })
-    )
-
+    render.addTaskHtml(task, btnData);
   }
-
 }
-// task.find((el)=>{
-//   if(el.id === 1){
-//     return true;
-//   }
-// });
-// task.find(({id})=>id === 1);
-const addDescription = (taskobj) => {
-  if (auth) {
-    let inputAddTask = document.querySelectorAll(`.task-items .task-item`);
-    let task = taskobj;
-    inputAddTask.forEach(Element => {
-      Element.addEventListener('dblclick', function test(e) {
-        task.forEach((Element1) => {
-          if (Number(Element1.id) === Number(Element.dataset.id)) {
-
-            let changeDiscription = document.querySelector(`.changeDiscription`);
-            let changeDiscriptionTextarea = document.querySelector(`.changeDiscription .changeTextare`);
-            let changeDiscriptionText = document.querySelector(`.changeDiscription .changeDiscriptionText`);
-            let nameTask = document.querySelector(`.changeDiscription .nameTask`);
-            let taskTextSubmit = document.querySelector(`.task-text-submit`);
-            let close = document.querySelector(`.close`);
-            let changeDiscriptionTextBtn = document.querySelector(`.changeDiscription .changeDiscriptionTextBtn`);
-            changeDiscriptionTextarea.value = '';
-            changeDiscription.style.display = 'flex';
-            nameTask.textContent = Element1.name;
-
-
-            if (Element1.text != '') {
-
-              changeDiscriptionText.textContent = Element1.text;
-              changeDiscriptionText.style.display = 'block';
-
-              changeDiscriptionTextarea.value = '';
-            }
-            close.addEventListener('click', () => {
-              changeDiscription.style.display = 'none';
-              changeDiscriptionText.textContent = '';
-              nameTask.textContent = '';
-
-
-            })
-            taskTextSubmit.addEventListener('click', function test2(e) {
-              Element1.text = changeDiscriptionTextarea.value;
-              e.currentTarget.removeEventListener('click', test2);
-
-            })
-
-
-          }
-
-
-        })
-
-        // e.currentTarget.removeEventListener('dblclick',test);
-
-      })
-
-
-    })
-
-  }
-
-}
-
 const transferTask = (task, datTask) => {
-
-  let taskText;
   if (auth) {
-    let inputAddTask = document.querySelectorAll('.task-submit');
-    let selectedTask = document.querySelector(`.task-items[data-state=${datTask}] .task-item_select`);
-    let innerTask = document.querySelectorAll(`.task-items`);
-    let addCard = document.querySelector(`.add-card[data-btn = ${datTask}]`)
-
-    inputAddTask.forEach(Element => {
-      Element.addEventListener('click', function changeStateHandler(e) {
-        clearAll(innerTask);
-        taskText = selectedTask.value;
-        let taskTextId = selectedTask.options[selectedTask.selectedIndex].dataset.id;
-        task.forEach((Element2) => {
-
-          if (Element2.id == taskTextId) {
-            Element2.state = datTask;
-          }
-        }
-
-        )
-
-        task.forEach(Element1 =>
-          addAllHtmlTask(Element1)
-        )
-        console.log(task);
-        Element.classList.add('task-submit_hidden');
-        selectedTask.parentElement.remove();
-        addCard.classList.remove('task-submit_hidden');
-        e.currentTarget.removeEventListener('click', changeStateHandler);
-        addDescription(task);
-      }, false)
-
-    })
+    render.transferTask(task, datTask);
   }
 }
-const clearAll = (htmlContent) => {
-  htmlContent.forEach(Element =>
-    Element.innerHTML = ''
-  );
-}
+
 const addTasks = (takeAll) => {
-  takeAll.forEach(Element =>
-    addAllHtmlTask(Element)
-  )
+  render.addTasks(takeAll);
 }
 regBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -155,44 +39,8 @@ regBtn.addEventListener("click", function (e) {
   regUser(User, login, password);
 
 })
-const addNewHtmlTask = (task) => {
-  let innerTask = document.querySelector(`.task-items[data-state = "${task.state}"] `);
-  let newTask = document.createElement('li');
-  newTask.className = "task-item";
-  newTask.innerHTML = task.name;
-  newTask.dataset.id = task.id;
-  innerTask.append(newTask);
-
-
-}
-const addAllHtmlTask = (task) => {
-  console.log(task);
-  let innerTask = document.querySelector(`.task-items[data-state = "${task.state}"] `);
-  console.log(innerTask);
-  let newTask = document.createElement('li');
-  newTask.className = "task-item";
-  newTask.dataset.id = task.id;
-  newTask.innerHTML = task.name;
-  innerTask.append(newTask);
-
-
-
-}
 const addHtmlSelected = (task, stateObj, prevElement) => {
-  let innerTask = document.querySelector(`.task-items[data-state = "${prevElement}"] `);
-  let newTask = document.createElement('li');
-  let stateTask = ``;
-  task.forEach((Element) => {
-
-    newTask.className = "task-item";
-
-    if (Element.state == stateObj) {
-      stateTask += ` <option class="task-item_option" data-id="${Element.id}" value="${Element.name}">${Element.name}</option>`;
-    }
-  });
-  newTask.innerHTML = `<select class="task-item_select" name="" id="">${stateTask}</select>`;
-
-  innerTask.append(newTask);
+  render.addHtmlSelected(task, stateObj, prevElement);
 }
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
